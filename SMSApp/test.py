e3deb15129994c55991acca1c8b776ad0ccb01d6
Sidmit0611 <siddhantmittal1001@gmail.com>
2026-03-11 00:16:11 +0530

    Update Google Sheets sheet_id in test.py to point to the correct spreadsheet

--- SMSApp/test.py
+++ SMSApp/test.py
@@ -8,7 +8,7 @@ scopes = [
 creds = Credentials.from_service_account_file("smart-expense-tracker-creds.json", scopes=scopes)
 client = gspread.authorize(creds)
 
-sheet_id = "1PVJXZ86PE8Q4ZLeB5l245aLYUkQlteKPwp3Cyb4PQgc"
+sheet_id = "1WeJECZJijBNH3WVABLoKqrNroSzIzW5qGo18T3QW-W4"
 
 sheet = client.open_by_key(sheet_id)
 values_list = sheet.sheet1.row_values(1)
